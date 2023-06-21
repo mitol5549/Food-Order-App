6e9e6f3de47cc5a5d4bd5400ce2a4931defacde7
Andrey Osipov <mitol5549@gmail.com>
2023-06-21 12:54:04 +0200

    CartIsShown State added.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
     <>
       <header className={classes.header}>
         <h1>Food Order App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
